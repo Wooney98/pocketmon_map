fa6fd2d6e4711cae1977e534acaa0c758a44373e
sws <109335745+Wooney98@users.noreply.github.com>
2022-12-07 18:50:03 +0900

    DB연동, urls경로지정, server연결 완.

--- pbook/models.py
+++ pbook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pocket(models.Model):
+    img_url = models.URLField()
+    name = models.TextField(max_length=10)
+    type = models.TextField(max_length=10)
+    height = models.CharField(max_length=10)
+    weight = models.CharField(max_length=10)
+    classify = models.TextField(max_length=10)
+    character = models.TextField(max_length=50) # 긴글
+    detail = models.TextField(max_length=50) # 물음표
--- pbook/views.py
+++ pbook/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from pbook.models import Pocket
 
 # Create your views here.
+def index(requests):
+    pockets = Pocket.objects.all().order_by("-name")
+    return render(requests, "index.html",{"pockets":pockets})
